2262ba206e6c01040c0fe7de41c1694c0f3135d9
leminhtuan <leminhtuan2015@gmail.com>
2015-01-26 11:04:46 +0700

    all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tuanmle::Application.config.secret_key_base = 'e279f7ca545cb37cf80827d467b04f1876b315bdfb49044ea5a5b1662c0c8722944010612890bd46384123f3819bdcf8212d000b79f0c1546bf94be5964ce2b7'
